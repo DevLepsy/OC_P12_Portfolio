2cb803dbb78dcb80c9eb2dce9638766bc9d47876
DevLepsy <henryck.paris@outlook.fr>
2024-04-30 17:11:52 +0200

    Fix HTML title and update logo in NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/devops.jpg";
+import logo from "../assets/devops.webp";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 
@@ -7,13 +7,7 @@ const NavBar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-srink-0 items-center">
-        <img
-          className="mx-2 w-10"
-          src={logo}
-          alt="logo"
-          width="200px"
-          heigth="200px"
-        />
+        <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
